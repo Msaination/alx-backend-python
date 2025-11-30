4fa97f4c43c483f95dbcc647add5867fcd90d91f
Sipho Nzilwane <64207098+Msaination@users.noreply.github.com>
2025-11-30 14:09:29 +0200

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()  # triggers post_delete signal
+    return redirect('home')  # redirect to homepage after deletion
