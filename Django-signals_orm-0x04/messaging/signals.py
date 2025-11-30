83970d06b150a71f1668a01fd2e18d0529412c0a
Sipho Nzilwane <64207098+Msaination@users.noreply.github.com>
2025-11-30 13:59:34 +0200

    Replace pre_save signal with post_save for notifications

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,19 +1,11 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from .models import Message, Notification
 
-@receiver(pre_save, sender=Message)
-def log_message_edit(sender, instance, **kwargs):
-    if instance.pk:  # only if updating an existing message
-        try:
-            old_message = Message.objects.get(pk=instance.pk)
-        except Message.DoesNotExist:
-            return
-        if old_message.content != instance.content:
-            # Save old content to history
-            MessageHistory.objects.create(
-                message=old_message,
-                old_content=old_message.content
-            )
-            # Mark message as edited
-            instance.edited = True
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
